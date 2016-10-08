ecc86e14f6c91c2c32de654bdc9f5b83d10c9b1b
bbbloke <hey@bbbloke.com>
2016-10-08 12:38:34 -0500

    Update bootstrap.sh

--- _vagrant/bootstrap.sh
+++ _vagrant/bootstrap.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD='12345678'
-PROJECTFOLDER='myproject'
+PASSWORD='pinupgirl16andbeyond'
+PROJECTFOLDER='ooolalababy'
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y apache2
-sudo apt-get install -y php5
+
+sudo apt-get install -y nginx
+sudo apt-get install -y php5-fpm
 
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
@@ -19,7 +20,7 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean 
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect nginx"
 sudo apt-get -y install phpmyadmin
 
 # Create project folder, written in 3 single mkdir-statements to make sure this runs everywhere without problems
@@ -28,23 +29,23 @@ sudo mkdir "/var/www/html"
 sudo mkdir "/var/www/html/${PROJECTFOLDER}"
 
 # setup hosts file
-VHOST=$(cat <<EOF
-<VirtualHost *:80>
-    DocumentRoot "/var/www/html/${PROJECTFOLDER}/public"
-    <Directory "/var/www/html/${PROJECTFOLDER}/public">
-        AllowOverride All
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOF
-)
+#VHOST=$(cat <<EOF
+#<VirtualHost *:80>
+#    DocumentRoot "/var/www/html/${PROJECTFOLDER}/public"
+#    <Directory "/var/www/html/${PROJECTFOLDER}/public">
+#        AllowOverride All
+#        Require all granted
+#    </Directory>
+#</VirtualHost>
+#EOF
+#)
 echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
 
 # enable mod_rewrite
-sudo a2enmod rewrite
+#sudo a2enmod rewrite
 
 # restart apache
-service apache2 restart
+service nginx restart
 
 # remove default apache index.html
 sudo rm "/var/www/html/index.html"
@@ -56,12 +57,12 @@ sudo apt-get -y install git
 sudo git clone https://github.com/panique/mini "/var/www/html/${PROJECTFOLDER}"
 
 # install Composer (not necessary by default)
-#curl -s https://getcomposer.org/installer | php
-#mv composer.phar /usr/local/bin/composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
 
 # go to project folder, load Composer packages (not necessary by default)
-#cd "/var/www/html/${PROJECTFOLDER}"
-#composer install --dev
+cd "/var/www/html/${PROJECTFOLDER}"
+composer install --dev
 
 # run SQL statements from MINI folder
 sudo mysql -h "localhost" -u "root" "-p${PASSWORD}" < "/var/www/html/${PROJECTFOLDER}/_install/01-create-database.sql"
